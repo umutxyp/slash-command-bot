65f1c25448eabbd1d789d9071bd8533073477305
Umut Bayraktar <69085038+1umutda@users.noreply.github.com>
2022-04-12 19:31:25 +0300

    Update example.js

--- commands/example.js
+++ commands/example.js
@@ -1,1 +1,19 @@
-a
+const Discord = require("discord.js");
+
+exports.run = async (client, message, args) => {
+
+    const embed = new Discord.MessageEmbed()
+    .setTitle("Command Title")
+    .setDescription("**https://discord.gg/6XGqdgE**")
+    .setColor("BLUE")
+    .setTimestamp()
+    return message.channel.send({embeds : [embed]});
+
+};
+exports.conf = {
+  aliases: []
+};
+
+exports.help = {
+  name: "test"
+};
