e8ff485e4bcb8d43ce1cff319705a2a8704915dc
Umut Bayraktar <69085038+1umutda@users.noreply.github.com>
2022-04-12 19:31:58 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 const client = require("../index");
 
 client.on("ready", () => {
-    console.log(`${client.user.tag} İsmi İle Bot Aktif!`)
+    console.log(`${client.user.tag} Bot Online!`)
     client.user.setActivity(`Umut Bayraktar ♥ Code Share`)
 });
