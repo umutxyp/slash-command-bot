0d9619efb90d3f6c4a5de581d70661c2d1b54585
Umut Bayraktar <69085038+1umutda@users.noreply.github.com>
2022-07-08 15:53:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,12 @@
-const { Client, Message, MessageEmbed, Collection } = require("discord.js");
-const fs = require("fs");
+const { Client, MessageEmbed } = require("discord.js");
 const config = require("./config.json");
 const token = config.token;
 
-
 const client = new Client({
-  messageCacheLifetime: 60,
-  fetchAllMembers: false,
-  messageCacheMaxSize: 10,
-  restTimeOffset: 0,
-  restWsBridgetimeout: 100,
-  shards: "auto",
-  partials: ["MESSAGE", "CHANNEL", "REACTION"],
-  intents: 32767,
+intents: 32767
 });
 module.exports = client;
 
 require("./events/message.js")
 require("./events/ready.js")
-
-client.commands = new Collection();
-client.aliases = new Collection();
-fs.readdir("./commands/", (err, files) => {
-  if (err) console.error(err);
-  console.log(`${files.length} Total Command!`);
-  files.forEach(f => {
-    let props = require(`./commands/${f}`);
-    console.log(`${props.help.name} Named Command Online!`);
-    client.commands.set(props.help.name, props);
-    props.conf.aliases.forEach(alias => {
-      client.aliases.set(alias, props.help.name);
-    });
-  });
-});
-
-
-
 client.login(token); 
