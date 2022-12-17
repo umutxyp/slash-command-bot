ef5f2fae04f1dd66db18150696db37efc30cb98f
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-12-17 22:12:49 +0300

    Update and rename message.js to interactionCreate.js

---
+++ events/interactionCreate.js
@@ -0,0 +1,23 @@
+const { InteractionType } = require("discord.js");
+const fs = require("fs");
+module.exports = async(client, interaction) => {
+if(!interaction.guild) return;
+if(interaction.user.bot) return;
+
+if (interaction.type === InteractionType.ApplicationCommand) {
+fs.readdir("./commands", (err, files) => {
+if (err) throw err;
+files.forEach(async (f) => {
+let props = require(`../commands/${f}`);
+if (interaction.commandName.toLowerCase() === props.name.toLowerCase()) {
+try {
+return props.run(client, interaction);
+} catch (e) {
+return interaction.reply({ content: `ERROR\n\n\`\`\`${e.message}\`\`\``, ephemeral: true }).catch(e => { })
+}
+}
+});
+});
+}
+
+}
